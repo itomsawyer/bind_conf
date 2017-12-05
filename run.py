b8599b70e838ae33855654827dc9f655357499af
itomsawyer <tomsawyer126@gmail.com>
2017-12-06 01:23:07 +0800

    runmode

--- run.py
+++ run.py
@@ -3,10 +3,10 @@ import os
 from config import app_config
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app("dev")
-lh = app_config["dev"].LISTEN_HOST
-lp = app_config["dev"].LISTEN_PORT
+runmode="dev"
+app = create_app(runmode)
+lh = app_config[runmode].LISTEN_HOST
+lp = app_config[runmode].LISTEN_PORT
 
 if __name__ == '__main__':
     app.run(host=lh, port=lp, threaded=True)
